3d449dd60daadd5a774020cb3e022efe1b7762ce
RyanCocking <bsrctb@leeds.ac.uk>
2022-02-09 14:58:58 +0000

    Further install script changes

--- install_ffea.sh
+++ install_ffea.sh
@@ -1,50 +1,93 @@
 #!/bin/bash
 # Author: Ryan Cocking (2022)
 # This script builds FFEA, assuming you have pre-compiled eigen3 and boost.
-do_cmake=${1:-0}
-do_install=${2:-0}
-do_ctest=${3:-0}
-restrict_tests=${3:-0}
 
+# ============= Parameters ============= #
 src_dir=$FFEA_SRC
 cmake_install_prefix=$FFEA_BUILD  # not sure if needed when we're already moving to that directory
 python_exec=$CONDA_PREFIX/bin/python  # if you're in the correct (python2.7) conda env, this may not be needed either
-boost_root_dir=$SOFTWARE_HOME/boost_build
-eigen3_include_dir=$SOFTWARE_HOME/eigen-3.3.9/include/eigen3
-
+boost_root_dir=$SOFTWARE_HOME/boost_build  # leave empty for FFEA to auto-download
+eigen3_include_dir=$SOFTWARE_HOME/eigen-3.3.9/include/eigen3  #  "   "   "    "
 echo ""
-echo "do_cmake = "$do_cmake
-echo "do_install = "$do_install
-echo "do_ctest = "$do_ctest
-echo "restrict_tests = "$restrict_tests
+echo "SRC                   "$src_dir
+echo "CMAKE_INSTALL_PREFIX  "$cmake_install_prefix
+echo "PYTHON_EXECUTABLE     "$python_exec
+echo "BOOST_ROOT            "$boost_root_dir
+echo "EIGEN3_INCLUDE_DIR    "$eigen3_include_dir
+
+if [[ -z $boost_root_dir ]];
+then
+    echo "Building with internal Boost"
+fi
+if [[ -z $eigen3_include_dir ]];
+then
+    echo "Building with internal Eigen3"
+fi
+
+# ================ Args ================ #
+do_cmake=${1:-0}  # required if making changes to the testing code
+do_install=${2:-0}
+do_ctest=${3:-0}
+restrict_tests=${3:-0}
+echo ""
+echo "do_cmake        "$do_cmake
+echo "do_install      "$do_install
+echo "do_ctest        "$do_ctest
+echo "restrict_tests  "$restrict_tests
 echo ""
 
-# CMake is required if making changes to the testing environment
+# ============== Threads =============== #
+max_proc_id=$(cat /proc/cpuinfo | grep 'processor' | tail -1 | awk '{print $3}')
+max_threads=$((max_proc_id+1))
+use_threads=$((max_threads/2))
+
+# Set use_threads to 1 if empty
+if [[ -z $use_threads ]];
+then
+    use_threads=1
+fi
+echo "Building with "$use_threads" threads out of "$max_threads
+echo ""
+
+# =============== Script =============== #
 if [[ $do_cmake -eq 1 ]];
 then
     rm -r $cmake_install_prefix
     mkdir $cmake_install_prefix
     cd $cmake_install_prefix
-    cmake $src_dir -DCMAKE_INSTALL_PREFIX=$cmake_install_prefix -DPYTHON_EXECUTABLE=$python_exec -DUSE_EIGEN3_INTERNAL=OFF -DUSE_BOOST_INTERNAL=OFF -DBOOST_ROOT=$boost_root_dir -DEIGEN3_INCLUDE_DIR=$eigen3_include_dir
-    make -j8
+
+    if [[ -z $boost_root_dir && -z $eigen3_include_dir ]];
+    then
+        cmake $src_dir -DCMAKE_INSTALL_PREFIX=$cmake_install_prefix -DPYTHON_EXECUTABLE=$python_exec -DUSE_EIGEN3_INTERNAL=ON -DUSE_BOOST_INTERNAL=ON
+    elif [[ -z $boost_root_dir && ! -z $eigen3_include_dir ]];
+    then
+        cmake $src_dir -DCMAKE_INSTALL_PREFIX=$cmake_install_prefix -DPYTHON_EXECUTABLE=$python_exec -DUSE_EIGEN3_INTERNAL=OFF -DUSE_BOOST_INTERNAL=ON -DEIGEN3_INCLUDE_DIR=$eigen3_include_dir
+    elif [[ ! -z $boost_root_dir && -z $eigen3_include_dir ]];
+    then
+        cmake $src_dir -DCMAKE_INSTALL_PREFIX=$cmake_install_prefix -DPYTHON_EXECUTABLE=$python_exec -DUSE_EIGEN3_INTERNAL=ON -DUSE_BOOST_INTERNAL=OFF -DBOOST_ROOT=$boost_root_dir
+    else
+        cmake $src_dir -DCMAKE_INSTALL_PREFIX=$cmake_install_prefix -DPYTHON_EXECUTABLE=$python_exec -DUSE_EIGEN3_INTERNAL=OFF -DUSE_BOOST_INTERNAL=OFF -DBOOST_ROOT=$boost_root_dir -DEIGEN3_INCLUDE_DIR=$eigen3_include_dir
+    fi
+
+    make -j $use_threads
 else
     cd $cmake_install_prefix
 fi
 	
 if [[ $do_install -eq 1 ]];
 then
-	make install -j8
+	make install -j $use_threads
 else
-    make -j8
+    make -j $use_threads
 fi
 
 if [[ $do_ctest -eq 1 ]];
 then
 	if [[ $restrict_tests -eq 1 ]];
 	then
-		ctest -j8 --verbose -R rod_neighbour_list_construction
+		ctest -j $use_threads --verbose -R rod_neighbour_list_construction
 	else
-		ctest -j8
+		ctest -j $use_threads
 	fi
 fi
 echo "Done"
