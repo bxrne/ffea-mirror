Increased force scaling factor defined in rod_structure. Adjusted PyMOL display radius as this still seemed to be incorrect - we know when the steric force is removed from the rods, so just edit the display radius until the rods are barely touching when this occurs. Fixed bug when loading in num_neighbours in FFEA_rod.

ffeatools/modules/FFEA_rod.py
@@ -306,7 +306,7 @@ class FFEA_rod:
                     row += 1
                     line = rod_file.readline()
                     self.num_neighbours[frame_no] = np.fromstring(
-                        line, sep=",").reshape(self.num_neighbours[frame_no].shape)
+                        line, sep=",", dtype=int)
                     frame_no += 1
                 except ValueError as e:
                     raise ValueError(str(e) + "\nError loading frame " +