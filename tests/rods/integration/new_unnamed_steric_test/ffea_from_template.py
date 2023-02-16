c80bb8a74fb590af1edc5b380eccd117b43484c2
RyanCocking <bsrctb@leeds.ac.uk>
2023-02-16 17:26:32 +0000

    Added docstrings and fixed string replacement

--- tests/rods/integration/new_unnamed_steric_test/ffea_from_template.py
+++ tests/rods/integration/new_unnamed_steric_test/ffea_from_template.py
@@ -1,7 +1,7 @@
+# Generate rod configurations and create FFEA input files from them
 # Ryan Cocking 2023
 from pathlib import Path
 import shutil
-import sys
 from omegaconf import OmegaConf
 
 
@@ -15,7 +15,9 @@ def copy_template(filename: str):
     return Path(filename)
 
 
-def write_test_config_yaml(r: float, l: float, config_filename: str):
+def write_test_config_yaml(r: str, l: str, config_filename: str):
+    """Write translations and rotations of a rod to a .yml file."""
+
     config = {
         "parallel": {
             "+x": [r, 0, 0, 0, 0, 0],
@@ -39,24 +41,32 @@ def write_test_config_yaml(r: float, l: float, config_filename: str):
     return config
 
 
-def edit_ffea_field(file_line: str, template_val: str, new_val: str) -> str:
-    """Edits a field string, e.g. "<foo = bar>", read from a .ffea file."""
+def edit_ffea_field(file_line: str, val_old: str, val_new: str) -> str:
+    """Edits a string of format "<foo = bar>" read from a .ffea file."""
+
     whitespace = file_line.split("<")[0]
     field = file_line.split("<")[-1].split(">")[0]  # assumes no comments in file
-    field.replace(template_val, new_val)
+    field = field.replace(val_old, val_new)
     return f"{whitespace:s}<{field:s}>\n"
 
 
-def edit_ffea_file(ffea_file_path: str, trans: tuple[float], rot: tuple[float]):
-    """ "Edits rod fields in a .ffea file in-place. Assumes template has a certain format."""
-    if len(trans) and len(rot) != 3:
+def edit_ffea_file(
+    ffea_file_path: str,
+    trans_old: str,
+    trans_new: tuple[float],
+    rot_old: str,
+    rot_new: tuple[float],
+):
+    """Edits in-place the rod traj, centroid, and rotation fields of a .ffea file."""
+
+    if len(trans_new) and len(rot_new) != 3:
         raise Exception("Translation and rotation vectors must be length 3")
 
     count = 1
     name = ffea_file_path.split("/")[-1].split(".")[0]
 
-    with open(ffea_file_path, "r") as fr:
-        lines = fr.readlines()
+    with open(ffea_file_path, "r") as f:
+        lines = f.readlines()
 
     for i, line in enumerate(lines):
         if "output" and "rodtraj" in line:
@@ -65,38 +75,47 @@ def edit_ffea_file(ffea_file_path: str, trans: tuple[float], rot: tuple[float]):
 
         if "centroid_pos" in line:
             lines[i] = edit_ffea_field(
-                line, "(0.0,0.0,0.0)", f"({trans[0]:.2f},{trans[1]:.2f},{trans[2]:.2f})"
+                line,
+                trans_old,
+                f"({trans_new[0]:.2f}, {trans_new[1]:.2f}, {trans_new[2]:.2f})",
             )
 
         if "rotation" in line:
             lines[i] = edit_ffea_field(
-                line, "(0.0,0.0,0.0)", f"({rot[0]:.2f},{rot[1]:.2f},{rot[2]:.2f})"
+                line, rot_old, f"({rot_new[0]:.2f}, {rot_new[1]:.2f}, {rot_new[2]:.2f})"
             )
 
-    with open(ffea_file_path, "w") as fw:
-        fr.writelines(lines)
+    with open(ffea_file_path, "w") as f:
+        f.writelines(lines)
 
     return lines
 
 
 def main():
-    # define rod configurations by hand, check in PyMOL
 
+    # Define rod configurations
     params = OmegaConf.load("params.yml")
     radius = params["radius"]
     length = params["length"]
     test_config = write_test_config_yaml(
-        radius, length, "test_configdihaiadshadjada.yml"
+        radius / params["ffea_length"],
+        length / params["ffea_length"],
+        "test_config.yml",
     )
 
+    # Generate .ffea files for colliding rod pairs
     for key1, val1 in test_config.items():
         for key2, val2 in val1.items():
 
             path = params["in_dir"] + f"/{key1}_{key2}.ffea"
             copy_template(path)
-
-            # in-line replace new .ffea file according to the config
-    pass
+            edit_ffea_file(
+                ffea_file_path=path,
+                trans_old="(0.0, 0.0, 0.0)",
+                trans_new=val2[:3],
+                rot_old="(0.0, 0.0, 0.0)",
+                rot=val2[3:],
+            )
 
 
 if __name__ == "__main__":
