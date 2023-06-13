20ced7ef869a909ce4cf2bc19be3882742e6ce04
RyanCocking <bsrctb@leeds.ac.uk>
2023-06-13 19:35:42 +0100

    Test for centroid wrapping of rods during periodic boundary conditions

--- tests/rods/integration/pbc_wrap/run_test.py
+++ tests/rods/integration/pbc_wrap/run_test.py
@@ -12,27 +12,47 @@ mpl.use('Agg')
 
 def main():
 
-    # subprocess.call(["bash", "cleanup.sh"])
-    # py_return_status = subprocess.call(["python", "create_straight_rod.py"])
+    subprocess.call(["bash", "cleanup.sh"])
+    py_return_status = subprocess.call(["python", "create_straight_rod.py"])
 
-    # if py_return_status != 0:
-    #     sys.exit("Error calling create_straight_rod.py")
+    if py_return_status != 0:
+        sys.exit("Error calling create_straight_rod.py")
 
-    # if len(glob.glob("*.ffea")) > 1:
-    #     sys.exit("Error: more than one .ffea file - cannot determine test "
-    #              "name")
-    # else:
-    #     ffea_script_name = glob.glob("*.ffea")[0].split(".")[0]
+    if len(glob.glob("*.ffea")) > 1:
+        sys.exit("Error: more than one .ffea file - cannot determine test "
+                 "name")
+    else:
+        ffea_script_name = glob.glob("*.ffea")[0].split(".")[0]
 
-    # # test function located in: src/ffea_test.cpp
-    # print("Calling FFEA from Python script")
-    # subprocess.call(["ffea", ffea_script_name + ".ffea"])
+    # test function located in: src/ffea_test.cpp
+    print("Calling FFEA from Python script")
+    subprocess.call(["ffea", ffea_script_name + ".ffea"])
 
-    # # load both rod trajectories
-    # print("Analysing output of FFEA simulation" + str("\n"))
-    # rod1 = ffea_rod.ffea_rod(filename="1A.rodtraj")
-    # rod2 = ffea_rod.ffea_rod(filename="1B.rodtraj")
+    # load both rod trajectories
+    print("Analysing output of FFEA simulation" + str("\n"))
+    rod1 = ffea_rod.ffea_rod(filename="1.rodtraj")
 
+    centre_node_r = rod1.current_r[:, 2, :]*1e9
+
+    box_dim_nm = 1176.47*1.7e-10*1e9
+    steps = np.arange(0,centre_node_r.shape[0])
+
+    plt.plot(steps, centre_node_r[:,0],'b-', label="x")
+    plt.plot(steps, centre_node_r[:,1],'r-', label="y")
+    plt.plot(steps, centre_node_r[:,2],'g-', label="z")
+    plt.plot(steps, np.zeros(steps.size), 'k--')
+    plt.plot(steps, np.ones(steps.size)*box_dim_nm, 'k--')
+    plt.xlabel("t (s)")
+    plt.ylabel("y (nm)")
+    plt.legend()
+    plt.savefig("xyzt_2d.png",dpi=400)
+
+    print(f"box_dim     : {box_dim_nm:e} nm")
+    print(f"y(step=95)  : {centre_node_r[95, 1]:e} nm")
+    print(f"y(step=105) : {centre_node_r[105, 1]:e} nm")
+    # a few steps either side of the crossing point (+ve y wall)
+    if (box_dim_nm/2 < centre_node_r[95, 1] < box_dim_nm) and (0 < centre_node_r[105, 1] < box_dim_nm/2):
+        return 0
     return 1
 
 
